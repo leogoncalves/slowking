b37a9d490fc96d6876743720ab9ae95194119807
Leonardo Gonçalves <leonardo.goncalves.2711@gmail.com>
2015-08-31 02:56:23 -0300

    Rewrite gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,82 +1,47 @@
 var gulp       = require('gulp'),
-    clean      = require('gulp-clean'),
+    gutil      = require('gulp-util');
     changed    = require('gulp-changed'),
     imagemin   = require('gulp-imagemin'),
     pngquant   = require('imagemin-optipng')
     less       = require('gulp-less'),
     concat     = require('gulp-concat'),
-    sourceMaps = require('gulp-sourcemaps')
     minifyCss  = require('gulp-minify-css'),
     notify     = require('gulp-notify'),
-    gutil      = require('gulp-util');
 
-gulp.task('copy-less', function(){
-    gulp.src('./src/components/bootstrap-less/less/**/*.less')
-    .pipe(gulp.dest('src/bootstrap/less/'))
-});
-
-gulp.task('copy-fonts', function(){
-    gulp.src('./src/components/bootstrap-less/fonts/*.{ttf,woff,eof,svg}')
-    .pipe(gulp.dest('src/bootstrap/fonts/'))
-});
-
-gulp.task('copy-js', function(){
-    gulp.src('./src/components/bootstrap-less/js/*.js')
-    .pipe(gulp.dest('src/bootstrap/js/'))
-});
+var src = '';
+var dist = '';
 
 gulp.task('compile-less', function(){
-    gulp.src('path/to/main.less')
+    gulp.src('./src/assets/less/main.less')
+    .pipe(changed('./dist/assets/css/*.{css}'))
     .pipe(less({compress: true}).on('error', gutil.log))
     .pipe(minifyCss({keepBreaks: false}))
-    .pipe(gulp.dest('dist'))
-    .pipe(notify("Compiled and compressed and minified "));
+    .pipe(gulp.dest('.dist/assets/css'))
+    .pipe(notify("Less files compiled"));
 });
 
-gulp.task('uglify', function(){
-    gulp.src('/folder/*.js')
+gulp.task('compile-js', function(){
+    gulp.src('./src/assets/js/*.js')
+    .pipe(changed('./dist/assets/js/*.{js}'))
     .pipe(uglify())
-    .pipe(gulp.dest('/path/'))
+    .pipe(gulp.dest('./dist/js/'))
+    .pipe(notify(".js files compiled"));
 });
 
-gulp.task('concat-and-minify-javascript', function(){
-    return gulp.src(['file.js', 'file2,js', '.....'])
-    .pipe(concat({path: 'scripts.js', stats: {mode: 0666}}))
-    .pipe(gulp.dest('/dist/js/'));
-});
+// gulp.task('concat-and-minify-javascript', function(){
+//     return gulp.src(['file.js', 'file2,js', '.....'])
+//     .pipe(concat({path: 'scripts.js', stats: {mode: 0666}}))
+//     .pipe(gulp.dest('/dist/js/'));
+// });
 
-gulp.task('generate-sourcemap', function(){
-    return gulp.src('/src/**/*.js')
-    .pipe(sourcemaps.init())
-    .pipe(concat('file.js'))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('destination'));
-});
-
-gulp.task('clean', function(){
-    return gulp.src('./src/components/')
-    .pipe(clean({force: true}))
-});
-
-gulp.task('jpg', function(){
-    gulp.src('./src/assets/images/*.{jpg}')
+gulp.task('compress-images', function(){
+    return gulp.src('./src/assets/images/*.{png,jpg}')
     .pipe(changed('./dist/assets/images/'))
+    .pipe(pngquant({optimizationLevel: 3})())
     .pipe(imagemin({
         progressive: true
     }))
-    .pipe(gulp.dest('./dist/assets/images/'))
-});
-
-gulp.task('png', function(){
-    return gulp.src('./src/assets/images/*.{png}')
-    .pipe(changed('./dist/assets/images/'))
-    .pipe(pngquant({optimizationLevel: 3})())
     .pipe(gulp.dest('./dist/assets/images/'));
 });
 
-
-gulp.task('arrange-workflow', [ 'copy-less', 'copy-fonts', 'copy-js' ]);
-gulp.task('compile-css-js', [ 'css', 'js' ] );
-gulp.task('minify-images', [ 'jpg', 'png' ]);
-
-//gulp.task('default', ['one', 'compress', 'linenos', 'sourcemaps-inline', 'sourcemaps-external']);
+gulp.task('compile', [ 'compile-less', 'compile-js', 'compress-images' ] );
